7e9d95feefedfbc10319403fba8d2c179bd1d986
Nick Lang <nl@polyswarm.io>
2018-07-02 17:07:07 -0700

    fix script

--- wait_and_run.sh
+++ wait_and_run.sh
@@ -3,7 +3,7 @@
 ./scripts/wait_for_it.sh $POLYSWARM_HOST:$POLYSWARM_PORT -t 0
 #sleep 5
 echo "starting signer..."
-python3 pythonSigner.py& # >/dev/null & 
+#python3 pythonSigner.py& # >/dev/null & 
 for i in `seq 1 10`
 do
 	python newAmbassador.py
