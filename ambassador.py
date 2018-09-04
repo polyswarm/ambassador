73733bdda9134327e3105e85fe8b63f75faa1d2a
Colin Seale <ceseale3@gmail.com>
2018-09-04 15:05:51 -0700

    add option to loop bounty posting

--- ambassador.py
+++ ambassador.py
@@ -34,19 +34,27 @@ BID = os.environ.get('BID','625000000000000000')
         help='Bid for the bounties you want to submit')
 @click.option('--duration', default=BOUNTY_DURATION,
         help='How long you want the bounties to run')
+@click.option('--restart', envvar='RESTART', default=None,
+        help='How many times would you like the script to retart submitting files')
 @click.option('--api-key', envvar='API_KEY', default=API_KEY,
         help='API key to use with polyswarmd')
 
-def main(log, polyswarmd_addr, keyfile, password, bounty_directory, bid, duration, api_key):
+def main(log, polyswarmd_addr, keyfile, password, bounty_directory, bid, duration, restart, api_key):
     loglevel = getattr(logging, log.upper(), None)
     priv_key = None
     address = None
 
     account = '0x' + json.loads(open(keyfile,'r').read())['address']
 
-    logging.debug('using account ' + ACCOUNT + "...")
+    logging.debug('using account + %s + ...', ACCOUNT)
+
 
     run_bounties(polyswarmd_addr, keyfile, password, bounty_directory, bid, duration, api_key, account)
+
+    if restart and int(restart) > 0:
+        for i in range(0, int(restart)):
+            logging.debug('restart: %s ...', i)
+            run_bounties(polyswarmd_addr, keyfile, password, bounty_directory, bid, duration, api_key, account)
 
     # not running offers atm because the introduction of websockets complicates e2e testing
     # to run standalone run `python offers.py`
